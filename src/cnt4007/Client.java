3c754d3b24dfbcaecbddda947dfdf24d756857ff
cseschmidt <113317598+cseschmidt@users.noreply.github.com>
2023-10-01 21:19:42 -0400

    Adding Client and Server files

---
+++ src/cnt4007/Client.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            String host = "127.0.0.1";
+            int port = 4444;
+            Socket socket = new Socket(host, port);
+            System.out.println("Connected to the server");
+
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            while ((line = userInput.readLine()) != null) {
+                writer.println(line);
+                String response = reader.readLine();
+                System.out.println("Server says: " + response);
+                if (line.equals("exit")) break;
+            }
+
+            socket.close();
+        } catch (UnknownHostException ex) {
+            System.out.println("Server not found: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("I/O Error: " + ex.getMessage());
+        }
+    }
+}
---
+++ src/cnt4007/Server.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            int port = 4444;
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server is running and listening on port " + port);
+            
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client connected: " + clientSocket.getInetAddress());
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                new Thread(clientHandler).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ClientHandler implements Runnable {
+    private final Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStream input = clientSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            OutputStream output = clientSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            String clientMessage;
+            while ((clientMessage = reader.readLine()) != null) {
+                System.out.println("Received: " + clientMessage);
+                if (clientMessage.equals("exit")) break;
+                writer.println("Server Received: " + clientMessage);
+            }
+
+            clientSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
